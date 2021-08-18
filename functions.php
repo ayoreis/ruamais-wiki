3edbc311a3b55be8d871466e62801df5bf8f76db
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-18 18:26:57 +0100

    Fixing names on add_actions

--- functions.php
+++ functions.php
@@ -150,8 +150,8 @@
         die();
     }
 
-    add_action('wp_ajax_data_fetch', 'data_fetch');
-    add_action('wp_ajax_nopriv_data_fetch', 'data_fetch');
+    add_action('wp_ajax_data_fetch', 'my_search');
+    add_action('wp_ajax_nopriv_data_fetch', 'my_search');
 
     function my_category_filter() {
 
