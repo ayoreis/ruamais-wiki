314795efc8a3cfed4f6993a5fd4521b72452da8b
Ayo Reis <ayo.v.reis@gmail.com>
2021-08-20 16:34:02 +0100

    Solved request error: 400 Bad Request

--- search.js
+++ search.js
@@ -3,36 +3,22 @@ const posts = document.querySelector('section.posts')
 
 search.addEventListener('input', () => {
 
-    // const request = new XMLHttpRequest()
-    // request.addEventListener('load', event => {
-    //     if (event.target.status === 200) {
-    //         posts.innerHTML = event.target.responseText
-    //     }
-    // })
-    //
-    // request.open('POST', './wp-admin/admin-ajax.php', true)
-    //
-    // request.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-    //
-    // request.send(`action=foo&valuesomething=hello`)
-    // &keyword=${search.value}
-
     const data = {
         action: 'data_fetch',
         keyword: `${search.value}`
     }
 
-    fetch('./wp-admin/admin-ajax.php', {
+    const request = fetch('./wp-admin/admin-ajax.php', {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         method: 'POST',
-        body: JSON.stringify(data)
+        body: `action=data_fetch&keyword=${search.value}`
     })
 
-    .then( request => request.text())
-    .then( request => console.log(request))
-    .catch(console.error)
+    request.then(respinse => response.text())
+    request.then(response => posts.innerHTML = response)
+    request.catch(console.error)
 
 })
 
